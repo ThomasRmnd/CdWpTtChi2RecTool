130282a1222363d5938ba695168991a6c1bbb29f
Thomas Raymond <thomas.raymond@iphc.cnrs.fr>
2025-07-19 10:53:08 +0200

    Complete strategies

--- src/predictor/fht/fht/WpFht.hpp
+++ src/predictor/fht/fht/WpFht.hpp
@@ -95,4 +95,72 @@ private:
 
 };
 
+template<>
+class NoGeomWpFht<ParamsType::DoubleAcrylic> final : public WpFht<ParamsType::DoubleAcrylic> {
+
+public:
+
+    ~NoGeomWpFht() final override = default;
+
+    double calculate(const RecPmtProp& pmt) {
+
+        // TODO when finished checking: Following this folloing paragraph in the setTrack method {
+
+        double d_i_wp_1 = 0.0, d_o_wp_1 = 0.0;
+        double d_i_wp_2 = 0.0, d_o_wp_2 = 0.0;
+        if (
+            !trajectoryHitCylinder(21750.0, 44000.0, m_orig_1, m_dir, d_i_wp_1, d_o_wp_1) ||
+            !trajectoryHitCylinder(21750.0, 44000.0, m_orig_2, m_dir, d_i_wp_2, d_o_wp_2)
+        ) { // need to be changed for a analytical solution
+            LogError << "Cannot project points into the WP\n";
+            LogError << "Orig: " << m_orig_1.x << ' ' << m_orig_1.y << ' ' << m_orig_1.z << '\n';
+            LogError << "Orig: " << m_orig_2.x << ' ' << m_orig_2.y << ' ' << m_orig_2.z << '\n';
+            LogError << "Dir: " << m_dir.x << ' ' << m_dir.y << ' ' << m_dir.z << '\n';
+            std::cin.get();
+            return 0.0;
+        }
+
+        vec3 end_wp_1 = m_orig_1 + m_dir * d_o_wp_1;
+        vec3 end_wp_2 = m_orig_2 + m_dir * d_o_wp_2;
+
+        // } end paragraph
+
+        // Cherenkov
+
+        double signed_pos = dot(m_dir, pmt.pos);
+
+        double fht_1 = 0.0, fht_2 = 0.0;
+
+        m_p_pmt_proj = m_orig_1 + dot(m_dir, pmt.pos - m_orig_1) * m_dir;
+        double dt_pmt_proj = dot(m_p_pmt_proj - m_orig_1, m_dir) * constants::inv_c;
+        double dt_1st_light = dt_pmt_proj - mag(pmt.pos - m_p_pmt_proj) * constants::inv_c_tan_cherenkov_w;
+        double d_1st_light = constants::c * dt_1st_light;
+        m_p_1st_light = m_orig_1 + d_1st_light * this->m_dir;
+    
+        fht_1 = m_t_0_1 + dt_1st_light + mag(pmt.pos - m_p_1st_light) * constants::inv_c_w;
+
+        m_p_pmt_proj = m_orig_2 + dot(m_dir, pmt.pos - m_orig_2) * m_dir;
+        double dt_pmt_proj = dot(m_p_pmt_proj - m_orig_2, m_dir) * constants::inv_c;
+        double dt_1st_light = dt_pmt_proj - mag(pmt.pos - m_p_pmt_proj) * constants::inv_c_tan_cherenkov_w;
+        double d_1st_light = constants::c * dt_1st_light;
+        m_p_1st_light = m_orig_2 + d_1st_light * this->m_dir;
+
+        fht_2 = m_t_0_2 + dt_1st_light + mag(pmt.pos - m_p_1st_light) * constants::inv_c_w;
+
+        if (0 < signed_pos) {
+            fht_1 = m_t_0_1 + d_o_wp_1 * constants::inv_c + mag(pmt.pos - end_wp_1) * constants::inv_c_w /* -10.0 */; // 10 is a constant, maybe because of the time emission
+            fht_2 = m_t_0_2 + d_o_wp_2 * constants::inv_c + mag(pmt.pos - end_wp_2) * constants::inv_c_w /* -10.0 */;
+        }
+        return std::min(fht_1, fht_2);
+    };
+
+private:
+
+    vec3 m_p_pmt_proj, m_p_1st_light;
+
+    double m_dist_1st_light;
+    double m_dist_orig_to_pmt_perp;
+
+};
+
 #endif // CDWPTTCHI2RECTOOL_PREDICTOR_FHT_FHT_WPFHT_HPP_
