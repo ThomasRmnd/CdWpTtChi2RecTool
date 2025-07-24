e51d4d5964372091c54c3e765dc49a9cce0aa402
Thomas Raymond <thomas.raymond@iphc.cnrs.fr>
2025-07-24 13:52:27 +0200

    Try.

--- src/estimator/fht/map/file/CorrectionFile.cpp
+++ src/estimator/fht/map/file/CorrectionFile.cpp
@@ -1,11 +1,20 @@
 #include "estimator/fht/map/file/CorrectionFile.hpp"
 
+#include <TROOT.h>
+
 #include "SniperKernel/SniperLog.h"
 
 CorrectionFile::MapType CorrectionFile::s_cache;
 
 CorrectionFile::CorrectionFile(const std::string& filename) {
-    m_file.reset(TFile::Open(filename.c_str(), "READ"));
+    TFile* f = TFile::Open(filename.c_str(), "READ");
+    if (!f || f->IsZombie()) {
+        LogError << "Failed to open file: " << filename;
+        m_file.reset();
+        return;
+    }
+    gROOT->GetListOfFiles()->Remove(f);
+    m_file.reset(f);  
 }
 
 std::shared_ptr<CorrectionFile> CorrectionFile::open(const std::string& filename) {
