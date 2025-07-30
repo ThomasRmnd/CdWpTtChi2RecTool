4cbd7ba637636196890a1c1a4bcf89b9897934ee
Thomas Raymond <thomas.raymond@iphc.cnrs.fr>
2025-07-30 19:40:27 +0200

    Try.

--- share/run.py
+++ share/run.py
@@ -82,7 +82,10 @@ rec_alg.property("PmtTTTimeReso").set(2.0)
 rec_alg.property("Use3inchPMT").set(True)
 rec_alg.property("Use20inchPMT").set(True)
 rec_alg.property("ChosenDetectors").set(3) # 1: CD, 2: WP, 4: TT
-# rec_alg.property("TimeDiffEventThreshold").set(300.0) # ~200.0 for Joint should be ok
+
+rec_alg.property("UseJointLoader").set(True)
+rec_alg.property("LoaderTimeWindow").set([-350.0, 350.0])
+
 rec_alg.rectool.property("UseAutomaticFactory").set(args.use_auto_factory)
 rec_alg.rectool.property("WaterPhase").set(args.water_phase)
 rec_alg.rectool.property("ManualReconstructionMode").set(args.manual_reconstruction_mode)
