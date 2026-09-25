3c6bdb2a1256a3a4642417b9f049a6f53b94586b
Thomas Raymond <thomas.raymond@iphc.cnrs.fr>
2026-09-25 11:36:35 +0200

    Fixed bug

--- src/trigger/TriggerTimeCorrelation.cpp
+++ src/trigger/TriggerTimeCorrelation.cpp
@@ -10,8 +10,73 @@ void TriggerTimeCorrelation::configure(const SniperJSON& config) {
 }
 
 bool TriggerTimeCorrelation::correct(RecPmtTable& table, JM::NavBuffer* buf) {
-    LogDebug << "Navigation buffer: " << buf << '\n';
-    (void)table;
-    std::cin.get();
+    if (!buf) {
+        LogError << "NavBuffer is nullptr\n";
+        return false;
+    }
+    JM::EvtNavigator* nav = buf->curEvt();
+    if (!nav) {
+        LogError << "Current EvtNavigator is nullptr\n";
+        return false;
+    }
+
+    m_cur_ts = TimeStamp{nav->TimeStamp().GetTimeSpec()};
+    m_cur_type = getDetectorType(nav);
+    if (m_cur_type == DetectorType::UNKNOWN) {
+        LogInfo << "Unexpected entry type (" << nav->getDetectorType() << ") at " << m_cur_ts << ". Ignoring\n";
+        return false;
+    }
+
+    for (JM::NavBuffer::Iterator it = buf->begin(); it != buf->end(); ++it) {
+        if (it == buf->current()) continue;
+        nav = it->get();
+        if (!nav) {
+            LogError << "EvtNavigator is nullptr\n";
+            return false;
+        }
+        m_other_ts = TimeStamp{nav->TimeStamp().GetTimeSpec()};
+        m_other_type = getDetectorType(nav);
+        if (m_other_type == DetectorType::UNKNOWN) {
+            LogInfo << "Unexpected entry type (" << nav->getDetectorType() << ") at " << m_other_ts << ". Ignoring\n";
+            continue;
+        }
+        LogDebug << "Current entry: (type: " << static_cast<int>(m_cur_type) << ", ts: " << m_cur_ts << ")\n";
+        LogDebug << "Other entry: (type: " << static_cast<int>(m_other_type) << ", ts: " << m_other_ts << ")\n";
+        if ( (m_cur_type & m_other_type) != DetectorType::UNKNOWN ) continue; // current event has already assotiated an event with same type
+        TimeStamp diff_ts = m_cur_ts - m_other_ts;
+        double diff_ts_ns = static_cast<double>(diff_ts.GetSec()) * 1.0e9 + static_cast<double>(diff_ts.GetNanoSec());
+        if (diff_ts_ns < m_time_window.first || m_time_window.second < diff_ts_ns) continue;
+        LogInfo << "Entries are associated: " << m_cur_ts << " - " << m_other_ts << " = " << diff_ts_ns << " ns\n";
+
+        changeRefTime(table, diff_ts_ns);
+        m_cur_type |= m_other_type;
+    }
+
     return true;
+}
+
+DetectorType TriggerTimeCorrelation::getDetectorType(JM::EvtNavigator* nav) {
+    DetectorType type = DetectorType::UNKNOWN;
+
+    JM::EvtNavigator::DetectorType evt_type = nav->getDetectorType();
+
+    if (evt_type == JM::EvtNavigator::DetectorType::CD) type |= DetectorType::CD;
+    if (evt_type == JM::EvtNavigator::DetectorType::WP) type |= DetectorType::WP;
+    if (evt_type == JM::EvtNavigator::DetectorType::TT) type |= DetectorType::TT;
+
+    return type;
+}
+
+void TriggerTimeCorrelation::changeRefTime(RecPmtTable& table, double diff_ts) {
+    if ( (m_other_type & DetectorType::CD) == DetectorType::CD ) changeRefTimeInRange(table, diff_ts, RecPmtType::PMT_CD);
+    if ( (m_other_type & DetectorType::WP) == DetectorType::WP ) changeRefTimeInRange(table, diff_ts, RecPmtType::PMT_WP);
+    if ( (m_other_type & DetectorType::TT) == DetectorType::TT ) changeRefTimeInRange(table, diff_ts, RecPmtType::PMT_TT);
+}
+
+void TriggerTimeCorrelation::changeRefTimeInRange(RecPmtTable& table, double diff_ts, const RecPmtType& type) {
+    for (RecPmtProp& pmt : table) {
+        if (!pmt.used || !hasPmtType(pmt, type)) continue;
+        LogDebug << "PMT (" << pmt.id << " = " << pmt.fht << " --> " << pmt.fht - diff_ts << '\n';
+        pmt.fht -= diff_ts;
+    }
 }
--- src/trigger/TriggerTimeCorrelation.hpp
+++ src/trigger/TriggerTimeCorrelation.hpp
@@ -1,10 +1,49 @@
 #ifndef CDWPTTCHI2RECTOOL_UTILS_TRIGGERTIMECORRELATION_HPP_
 #define CDWPTTCHI2RECTOOL_UTILS_TRIGGERTIMECORRELATION_HPP_
 
+#include "Context/TimeStamp.h"
 #include "EvtNavigator/NavBuffer.h"
 
 #include "utils/Configurable.hpp"
 #include "utils/RecPmtProp.hpp"
+
+enum class DetectorType {
+    UNKNOWN = 0,
+    CD = 1 << 0,
+    WP = 1 << 1,
+    TT = 1 << 2
+};
+
+inline constexpr DetectorType operator|(DetectorType lhs, DetectorType rhs) noexcept {
+    return static_cast<DetectorType>(static_cast<int>(lhs) | static_cast<int>(rhs));
+}
+
+inline constexpr DetectorType operator&(DetectorType lhs, DetectorType rhs) noexcept {
+    return static_cast<DetectorType>(static_cast<int>(lhs) & static_cast<int>(rhs));
+}
+
+inline constexpr DetectorType operator^(DetectorType lhs, DetectorType rhs) noexcept {
+    return static_cast<DetectorType>(static_cast<int>(lhs) ^ static_cast<int>(rhs));
+}
+
+inline constexpr DetectorType operator~(DetectorType rhs) noexcept {
+    return static_cast<DetectorType>(~static_cast<int>(rhs));
+}
+
+inline constexpr DetectorType& operator|=(DetectorType& lhs, DetectorType rhs) noexcept {
+    lhs = lhs | rhs;
+    return lhs;
+}
+
+inline constexpr DetectorType& operator&=(DetectorType& lhs, DetectorType rhs) noexcept {
+    lhs = lhs & rhs;
+    return lhs;
+}
+
+inline constexpr DetectorType& operator^=(DetectorType& lhs, DetectorType rhs) noexcept {
+    lhs = lhs ^ rhs;
+    return lhs;
+}
 
 class TriggerTimeCorrelation : public Configurable {
 
@@ -21,6 +60,16 @@ public:
 private:
 
     std::pair<double, double> m_time_window;
+
+    TimeStamp m_cur_ts;
+    TimeStamp m_other_ts;
+
+    DetectorType m_cur_type;
+    DetectorType m_other_type;
+
+    DetectorType getDetectorType(JM::EvtNavigator* nav);
+    void changeRefTime(RecPmtTable& table, double diff_ts);
+    void changeRefTimeInRange(RecPmtTable& table, double diff_ts, const RecPmtType& type);
 
 };
 
